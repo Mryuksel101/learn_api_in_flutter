d1bcaecb073c9b559af6b39f8330c644580b4db4
mustafa <mryuksel5@gmail.com>
2022-12-20 20:36:04 +0300

    uı

--- lib/translate_screen.dart
+++ lib/translate_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 class TranslateScreen extends StatefulWidget {
   const TranslateScreen({Key? key}) : super(key: key);
 
@@ -9,6 +13,7 @@ class TranslateScreen extends StatefulWidget {
 }
 
 class _TranslateScreenState extends State<TranslateScreen> {
+  
   TextEditingController textEditingController = TextEditingController();
   @override
   void initState() {
@@ -16,6 +21,38 @@ class _TranslateScreenState extends State<TranslateScreen> {
     super.initState();
 
   }
+
+  void wordMeaning()async{ //For us to use await, the parent function must be async. (The name of the parent function in our example is wordMeaning )
+    // http.post method is an asynchronous function
+    // So we use await before it is called
+
+    /*
+      In Flutter, the http.post method is used to send an HTTP POST request to a server. It takes two required arguments: the URL to send the request to, and the body of the request.
+    */
+    Response response = await http.post(
+      Uri.parse("https://google-translate1.p.rapidapi.com/language/translate/v2/detect"),
+        headers: { // In the context of an HTTP POST request, headers can be used to provide information about the request payload
+        "content-type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Key": "f8b372e531msh197b796b14029d6p172a34jsn5022c69f1603",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      }
+    );
+    // http.post function returns a Future<Response> object, which represents the response from the server
+
+    // The Response object contains information about the response, including the status code, headers, and the body of the response
+    // Data is often transmitted over the Internet in the form of text. so our response.body is text. Also called json string.
+
+    Map<String, dynamic> responseBody = json.decode(response.body);
+    // json.decode is a function from the dart:convert library that is used to parse a JSON string and convert it into a Dart object.
+    // The json.decode function takes a JSON string as input and returns the corresponding Dart object.
+
+
+    
+
+  } 
+
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
